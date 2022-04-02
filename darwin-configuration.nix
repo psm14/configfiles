4ca5e75313d7d42f0ee04ddbda7fe2568c46ab65
Pat McLaughlin <me@patmclaughl.in>
2022-04-02 16:39:25 -0400

    Split out home.nix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -55,61 +55,7 @@
     name = "pat";
     home = "/Users/pat";
   };
-  home-manager.users.pat = { config, pkgs, ... }: {
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "21.11";
-
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
-
-    home.packages = with pkgs; [
-      comma
-      bat
-      direnv
-      gitAndTools.delta
-      neovim
-    ];
-
-    programs.zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-      shellAliases = {
-        vim = "nvim";
-        ls = "ls --color=auto";
-	cat = "bat -pp";
-      };
-
-      oh-my-zsh = {
-        enable = true;
-	plugins = [ "git" ];
-	theme = "gallois";
-      };
-
-      initExtra = ''
-        export DIRENV_LOG_FORMAT=
-        export EDITOR=nvim
-      '';
-
-      initExtraBeforeCompInit = ''
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      '';
-    };
-
-    programs.direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-    };
-  };
+  home-manager.users.pat = import ./home.nix { isDarwin = true; };
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
