7aaf240636343ad5c6119e0f0bc464d7d73ebb4e
Pat McLaughlin <me@patmclaughl.in>
2022-04-04 17:01:40 -0400

    fixup

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? <nixpkgs>
-, pkgs ? import nixpkgs { inherit system; }
-, lib ? pkgs.lib,
+, pkgs ? import nixpkgs
+, lib ? pkgs.lib
  }: {
    home = import "./home.nix" { inherit pkgs; };
    darwin = import "./darwin-configuration.nix" { inherit pkgs lib; };
