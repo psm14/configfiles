7eb392a5b64ea59e7da3fb560f5b5cd1b162580f
Patrick McLaughlin <me@patmclaughl.in>
2025-01-17 18:46:34 -0500

    fix nvim lockfile in nix issue

--- nvim/lua/psm14/lazy_init.lua
+++ nvim/lua/psm14/lazy_init.lua
@@ -13,5 +13,6 @@ vim.opt.rtp:prepend(lazypath)
  
 require("lazy").setup({
     spec = "psm14.lazy",
-    change_detection = { notify = false }
+    change_detection = { notify = false },
+    lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json", -- move lockfile to XDG_DATA_HOME because nix store is readonly
 })
