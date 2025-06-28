92ea7f978176e971ba4d27ec48693c2f41df4477
Patrick McLaughlin <me@patmclaughl.in>
2025-06-28 11:17:46 -0400

    remove toggleterm-aider

--- nvim/lua/psm14/init.lua
+++ nvim/lua/psm14/init.lua
@@ -32,12 +32,6 @@ end
 vim.keymap.set('n', '<D-j>', _G.toggle_term, { desc = 'Toggle terminal' })
 vim.keymap.set('t', '<D-j>', '<C-\\><C-n>:lua toggle_term()<CR>', { desc = 'Toggle terminal' })
 
-require('toggleterm-aider').setup({
-  toggle_key = '<D-k>',
-  add_key = '<leader>aa',
-  drop_key = '<leader>ad',
-})
-
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
--- nvim/lua/psm14/lazy/toggleterm-aider.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'psm14/toggleterm-aider',
-  dependencies = {
-    'akinsho/toggleterm.nvim',
-    'nvim-tree/nvim-tree.lua'
-  },
-  config = true,
-}
